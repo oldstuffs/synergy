d1fe3dc7a5e8c03f30008c857da04a798b39b7cd
Hasan Demirtaş <utsukushihito@outlook.com>
2021-04-10 19:37:57 +0300

    Update Bootstrap.java

--- client/src/main/java/io/github/portlek/synergy/client/Bootstrap.java
+++ client/src/main/java/io/github/portlek/synergy/client/Bootstrap.java
@@ -61,7 +61,7 @@ public final class Bootstrap {
   public static final String VERSION = "1.0.0-SNAPSHOT";
 
   /**
-   * the ascii art for Snergy Client text.
+   * the ascii art for Synergy Client text.
    */
   private static final String ART = "" +
     "(  ____ \\|\\     /|( (    /|(  ____ \\(  ____ )(  ____ \\|\\     /|\n" +
