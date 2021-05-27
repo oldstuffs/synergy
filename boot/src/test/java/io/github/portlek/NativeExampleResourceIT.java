73f0583eb3669065e4b2fe411692f8474280add6
Hasan Demirtaş <utsukushihito@outlook.com>
2021-05-27 20:01:53 +0300

    remove.

--- boot/src/test/java/io/github/portlek/NativeExampleResourceIT.java
+++
@@ -1,8 +0,0 @@
-package io.github.portlek;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-  // Execute the same tests but in native mode.
-}
